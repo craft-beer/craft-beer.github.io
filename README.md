7db34f9a1a9325420d2a2ad88ea24e369b19a2eb
IgorArt <weblab.imporg@gmail.com>
2021-11-29 00:05:23 +0300

    init project

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# crafting-beer-table
+# CraftingBeerTable
+
+### Development build:
+```
+yarn install
+yarn run watch
+```
+
+Go to http://localhost:3000
+
+### Production build:
+```
+yarn install
+yarn run prod
+```
+
+Connect GitHub Pages to */docs*
---
+++ webpack.mix.js
@@ -0,0 +1,34 @@
+const mix = require('laravel-mix');
+
+const buildDir = mix.inProduction() ? 'docs' : 'build'
+
+mix.browserSync({
+    proxy: false,
+    open: false,
+    notify: false,
+    files: ['src/**/*'],
+    server: './build',
+})
+
+mix.disableSuccessNotifications()
+
+mix.options({
+    processCssUrls: true,
+    terser: {
+        extractComments: false,
+    },
+})
+
+mix.setPublicPath(buildDir)
+
+mix.js('src/js/app.js', buildDir + '/js')
+    .sass('src/scss/app.scss', buildDir + '/css')
+    .copyDirectory('src/images', buildDir + '/images')
+    .copy('src/index.html', buildDir)
+    .copy('src/storage.json', buildDir)
+
+if (mix.inProduction()) {
+    mix.version()
+} else {
+    mix.sourceMaps()
+}
