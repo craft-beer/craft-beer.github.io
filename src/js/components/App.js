2d0cb8380b5df717fe574b0b4812902284f01a3a
IgorArt <weblab.imporg@gmail.com>
2021-11-29 19:34:41 +0300

    get storage disabled cache

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -18,7 +18,7 @@ export default () => ({
     },
 
     loadStorage() {
-        return fetch(this.storageUrl)
+        return fetch(this.storageUrl + '?t=' + (Date.now() % 1000) / 1000)
             .then(res => res.json())
             .then(json => json)
             .catch(err => console.error(err))
