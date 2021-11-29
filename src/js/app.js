f9c76ee4cb7a2b0c73560831eb7eb7a278a62e44
IgorArt <weblab.imporg@gmail.com>
2021-11-29 19:28:43 +0300

    load data from storage with alpine

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
 import Alpine from 'alpinejs'
+import App from './components/App'
 
 window.Alpine = Alpine
+
+Alpine.data('App', App)
 
 Alpine.start()
---
+++ src/js/components/App.js
@@ -0,0 +1,42 @@
+export default () => ({
+    storageUrl: 'storage.json',
+    storage: {},
+    loading: true,
+    loadingDelay: 1000,
+    columns: {
+        left: [],
+        right: [],
+    },
+
+    async init() {
+        this.storage = await this.loadStorage()
+
+        setTimeout(() => {
+            this.splitItemsByColumn()
+            this.loading = false
+        }, this.loadingDelay)
+    },
+
+    loadStorage() {
+        return fetch(this.storageUrl)
+            .then(res => res.json())
+            .then(json => json)
+            .catch(err => console.error(err))
+    },
+
+    splitItemsByColumn() {
+        if (!this.storage.items || !this.storage.items.length) {
+            return
+        }
+
+        const count = this.storage.items.length
+        const chunk = Math.ceil(count / 2)
+
+        this.columns.left = this.storage.items.slice(0, chunk)
+        this.columns.right = this.storage.items.slice(chunk, count)
+    },
+
+    formatNumber(value) {
+        return ('0' + value).slice(-2)
+    }
+})
