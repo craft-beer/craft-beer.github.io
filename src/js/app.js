93618b499cd6d77d6313c81f7144ea1991fc7480
IgorArt <weblab.imporg@gmail.com>
2021-12-02 01:28:09 +0300

    remove Alpine from global scope

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,6 @@ import BeerTable from './components/BeerTable'
 import Clock from './components/Clock'
 import Weather from './components/Weather'
 
-window.Alpine = Alpine
-
 Alpine.data('BeerTable', BeerTable)
 Alpine.data('Clock', Clock)
 Alpine.data('Weather', Weather)
