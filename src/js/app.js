0f9cfe84c198d4b3f94577ef9377750f15d8c842
IgorArt <weblab.imporg@gmail.com>
2021-12-02 01:25:05 +0300

    blank weather component

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 import Alpine from 'alpinejs'
 import App from './components/App'
 import Clock from './components/Clock'
+import Weather from './components/Weather'
 
 window.Alpine = Alpine
 
 Alpine.data('App', App)
 Alpine.data('Clock', Clock)
+Alpine.data('Weather', Weather)
 
 Alpine.start()
